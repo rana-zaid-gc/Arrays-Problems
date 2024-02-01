0ef394c47e93faae4d0a34d2f9d3c515be699848
rana zaid <itszaidrajpoot@gmail.com>
2024-02-01 23:12:41 +0500

    first commit

---
+++ array.js
@@ -0,0 +1,38 @@
+// QNo1:Find the average of the entire class by array?
+
+
+// let marks=[85,97,89,58,45,69]
+
+// let sum=0
+
+// for(let val of marks){
+//     sum+=val
+// }
+// let avg=sum/marks.length
+// console.log(`the avearge of class = ${avg}`)
+
+
+
+// QNo2:For a given array with prices of 5 item->[250,245,300,900,50] All the items have offer of 10 % off on them.change the array to store final price after appling offer.
+
+
+// By for of loop method
+// let items =[250,645,300,900,50]
+// let i=0
+// for(let val of items){
+    
+//     let offer=val/10
+//     items[i]=items[i]-offer
+//     console.log(`the value at index ${i} after offer = ${items[i]}`)
+//     i++
+// }
+
+// BY for loop method 
+
+// let items =[250,645,300,900,50]
+// for(let i=0;i<items.length;i++){
+//     offer=items[i]/10
+//     items[i]-=offer
+    
+// }
+// console.log(items);
